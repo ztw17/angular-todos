dc1c9710f4d28acbd3249ca9b9fd1c3e214794ef
Zach Weber <weberzt@gmail.com>
2020-09-01 17:59:12 -0500

    added todos and todo-item

---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // Set Dynamic Classes
+  setClasses() {
+    let classes = {
+      todo: true,
+      "is-completed": this.todo.completed
+    }
+    return classes;
+  }
+
+}
