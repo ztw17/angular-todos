2a629ee42239d6cbbfbe8bdf54d97373769de00c
Zach Weber <weberzt@gmail.com>
2020-09-02 13:57:22 -0500

    added completed toggle functionality

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Todo } from 'src/app/models/Todo';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todo-item',
@@ -9,7 +10,7 @@ import { Todo } from 'src/app/models/Todo';
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
 
-  constructor() { }
+  constructor(private todoService:TodoService) { }
 
   ngOnInit(): void {
   }
@@ -24,7 +25,12 @@ export class TodoItemComponent implements OnInit {
   }
 
   onToggle(todo) {
+    // Toggle in UI
     todo.completed = !todo.completed;
+    // Toggle on server
+    this.todoService.toggleCompleted(todo).subscribe(todo => {
+      console.log(todo)
+    });
   }
 
   onDelete(todo) {
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -3,6 +3,12 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Todo } from '../models/Todo';
 import { Observable } from 'rxjs';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    "Content-Type": "application/json"
+  })
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,7 +18,14 @@ export class TodoService {
 
   constructor(private http:HttpClient) { }
 
+  // Get Todos
   getTodos():Observable<Todo[]> {
    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
+  }
+
+  // Toggle Completed
+  toggleCompleted(todo: Todo):Observable<any> {
+    const url = `${this.todosUrl}/${todo.id}`
+    return this.http.put(url, todo, httpOptions);
   }
 }
